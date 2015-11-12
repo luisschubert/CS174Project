5709e26d345bae714249a4785f301a491dd1a492
marcokuang <marcojkuang@gmail.com>
2015-11-12 13:00:18 -0800

    experiment code

--- js/dragdrop.js
+++ js/dragdrop.js
@@ -9,7 +9,15 @@ function init()
 }
 
 function handleDropEvent( event, ui ) {
-    console.log("faggot!!!!");
     var draggable = ui.draggable;
     alert( 'The square with ID "' + draggable.attr('id') + '"has been added to the shopping cart!' );
+    $("#output-result").append("<br> "+draggable.attr('id') + "</br>");
+    deleteImage( ui.draggable);
+}
+
+function deleteImage( $item ) {
+    $item.fadeOut(function() {
+
+    });
+    //$item.appendTo("cart-area").fadeIn;
 }
